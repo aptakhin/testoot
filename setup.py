7cf26a19085c958acfedbb735b275229805804ff
Alexander Ptakhin <me@aptakhin.name>
2020-04-20 06:22:43 +0300

    Reformatted setup

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 import os
-import sys
-
-import setuptools  # Don't delete, hook for distutils
 
 from distutils.core import setup
 
-
 setup(
-	name="regress",
-	version="0.0.3",
-	long_description=open(os.path.join(os.path.dirname(__file__),
+    name="regress",
+    version="0.0.3dev",
+    long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.md")).read(),
-	long_description_content_type="text/markdown",
-	package_dir={"regress": "regress"},
-	packages=[
-		"regress",
-		"regress.ext",
-		"regress.impl",
-		"regress.impl.context",
-		"regress.impl.serializer",
-		"regress.impl.storage",
-	],
+    long_description_content_type="text/markdown",
+    package_dir={"regress": "regress"},
+    packages=[
+        "regress",
+        "regress.ext",
+        "regress.impl",
+        "regress.impl.context",
+        "regress.impl.serializer",
+        "regress.impl.storage",
+    ],
     author="Alexander Ptakhin",
     author_email="me@aptakhin.name",
     description="Library",
@@ -30,15 +26,15 @@ setup(
     project_urls={
         "Source Code": "https://github.com/aptakhin/regress",
     },
-	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Intended Audience :: Developers",
-		"License :: OSI Approved :: MIT License",
-		"Programming Language :: Python",
-		"Topic :: Software Development",
-		"Framework :: Pytest",
-		"Topic :: Software Development :: Testing",
-		"Topic :: Software Development :: Quality Assurance",
-		"Topic :: Software Development :: Testing :: Unit",
-	],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Framework :: Pytest",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing :: Unit",
+    ],
 )
